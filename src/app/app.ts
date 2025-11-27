81a75d7b196f9caf4c4e6363bbc38d84bc658b84
Subramanya Malleshi <SMalleshi@jmawireless.com>
2025-11-27 15:29:30 +0530

    UI issue fixes

--- src/app/app.ts
+++ src/app/app.ts
@@ -8,5 +8,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.scss'
 })
 export class App {
-  protected readonly title = signal('ui-genisso');
+  protected readonly title = signal('genisso-ui');
 }
